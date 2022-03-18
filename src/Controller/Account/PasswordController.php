593564233f561aa1f43d78b5b3bd49692f4b79c4
liohumb <helloliohumb@gmail.com>
2022-03-18 23:17:22 +0100

    mise en place du mot de passe oublié

--- src/Controller/Account/PasswordController.php
+++ src/Controller/Account/PasswordController.php
@@ -2,9 +2,15 @@
 
 namespace App\Controller\Account;
 
+use App\Classes\Mail;
+use App\Entity\ResetPassword;
+use App\Entity\User;
 use App\Form\Password\ChangeType;
+use App\Form\Password\ResetType;
+use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
@@ -51,9 +57,94 @@ class PasswordController extends AbstractController
             }
         }
 
-        return $this->render('account/password/index.html.twig', [
+        return $this->render('account/password/change.html.twig', [
             'form' => $form->createView(),
             'notification' => $notification
+        ]);
+    }
+
+    /**
+     * @Route("/mot-de-passe-oublie", name="reset_password")
+     * @param Request $request
+     * @return Response
+     */
+    public function reset(Request $request): Response
+    {
+        if ($this->getUser()) {
+            return $this->redirectToRoute('home');
+        }
+
+        if ($request->get('email')) {
+            $user = $this->entityManager->getRepository(User::class)->findOneByEmail($request->get('email'));
+
+            if ($user) {
+                $reset_password = new ResetPassword();
+                $reset_password->setUser($user);
+                $reset_password->setToken(uniqid());
+                $reset_password->setCreatedAt(new DateTime());
+
+                $this->entityManager->persist($reset_password);
+                $this->entityManager->flush();
+
+                $url = $this->generateUrl('update_password', ['token' => $reset_password->getToken()]);
+                $mail = new Mail();
+                $mail->send(
+                    $user->getEmail(),
+                    $user->getFirstname() . ' ' . $user->getLastname(),
+                    "Réinitialiser votre mot de passe sur L & A WoodFactory",
+                    "Bonjour " . $user->getFirstname() . ", <br><br>
+                            Vous avez demandé à réinitializer votre mot de passe sur le site L & A WoodFactory. <br><br><br>
+                            Merci de bien vouloir cliquer sur le lien suivant pour <br>
+                            <a href='https://127.0.0.1:8000". $url ."'>mettre à jour votre mot de passe</a>."
+                );
+
+                $this->addFlash('notice', "Vous allez recevoir dans quelques seconde un email avec la procédure pour réinitialiser votre mot de passe.");
+            } else {
+                $this->addFlash('notice', "Cette adresse email est inconnue.");
+            }
+        }
+
+        return $this->render('account/password/reset.html.twig');
+    }
+
+    /**
+     * @Route("/modifier-mon-mot-de-passe/{token}", name="update_password")
+     * @param Request $request
+     * @param $token
+     * @return Response
+     */
+    public function update(Request $request, $token, UserPasswordHasherInterface $hasher): Response
+    {
+        $reset_password = $this->entityManager->getRepository(ResetPassword::class)->findOneByToken($token);
+
+        if (!$reset_password) {
+            return $this->redirectToRoute('reset_password');
+        }
+
+        $now = new DateTime();
+
+        if ($now > $reset_password->getCreatedAt()->modify('+ 3 hour')) {
+            $this->addFlash('notice', "Votre demande de mot de passe à expiré. Merci de la renouveler.");
+            return $this->redirectToRoute('reset_password');
+        }
+
+        $form = $this->createForm(ResetType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $new_pwd = $form->get('new_password')->getData();
+            $password = $hasher->hashPassword($reset_password->getUser(), $new_pwd);
+            $reset_password->getUser()->setPassword($password);
+
+            $this->entityManager->flush();
+
+            $this->addFlash('notice', 'Votre mot de passe a bien été mis à jour.');
+
+            return $this->redirectToRoute('login');
+        }
+
+        return $this->render('account/password/update.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }
--- src/Form/Password/ResetType.php
+++ src/Form/Password/ResetType.php
@@ -3,6 +3,8 @@
 namespace App\Form\Password;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -11,7 +13,33 @@ class ResetType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('field_name')
+
+            ->add('new_password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'Le mot de passe et la confirmation doivent être identique.',
+                'label' => 'Votre nouveau mot de passe',
+                'required' => true,
+                'first_options' => [
+                    'label' => 'Votre nouveau mot de passe',
+                    'attr' => [
+                        'class' => 'form__input',
+                        'placeholder' => ' '
+                    ],
+                    'label_attr' => [
+                        'class' => 'form__label'
+                    ]
+                ],
+                'second_options' => [
+                    'label' => 'Confirmez votre nouveau mot de passe',
+                    'attr' => [
+                        'class' => 'form__input',
+                        'placeholder' => ' '
+                    ],
+                    'label_attr' => [
+                        'class' => 'form__label'
+                    ]
+                ]
+            ])
         ;
     }
 
