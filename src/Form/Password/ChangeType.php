b5abf4a9fcb7e301ff28bac9f438a000446d0c2a
liohumb <helloliohumb@gmail.com>
2022-03-18 18:19:45 +0100

    init form password change type

---
+++ src/Form/Password/ChangeType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form\Password;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ChangeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email')
+            ->add('roles')
+            ->add('password')
+            ->add('firstname')
+            ->add('lastname')
+            ->add('cguv')
+            ->add('token')
+            ->add('createdAt')
+            ->add('active')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
