3a40a66a6a1c32fda9464af80bdd0aacc30ff59b
liohumb <helloliohumb@gmail.com>
2022-03-25 11:44:00 +0100

    debut de la création de la gestion du panier

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Classes\Cart;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,12 +10,31 @@ use Symfony\Component\Routing\Annotation\Route;
 class CartController extends AbstractController
 {
     /**
-     * @Route("/cart", name="app_cart")
+     * @Route("/panier", name="cart")
      */
-    public function index(): Response
+    public function index(Cart $cart): Response
     {
-        return $this->render('cart/index.html.twig', [
-            'controller_name' => 'CartController',
-        ]);
+        dd($cart->get());
+        return $this->render('cart/index.html.twig');
+    }
+
+    /**
+     * @Route("/panier/ajout/{id}", name="cart_add")
+     */
+    public function add($id, Cart $cart): Response
+    {
+        $cart->add($id);
+
+        return $this->redirectToRoute('cart');
+    }
+
+    /**
+     * @Route("/panier/supprimer", name="cart_remove")
+     */
+    public function remove(Cart $cart): Response
+    {
+        $cart->remove();
+
+        return $this->redirectToRoute('product');
     }
 }
