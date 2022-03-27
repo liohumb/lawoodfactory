c3cd2e703fb105fb9f37b9a6cf323a7f54ab2a09
liohumb <helloliohumb@gmail.com>
2022-03-27 11:56:24 +0200

    mise en place de la gestioin du panier

--- src/Classes/Cart.php
+++ src/Classes/Cart.php
@@ -2,18 +2,46 @@
 
 namespace App\Classes;
 
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class Cart
 {
     private SessionInterface $session;
+    private EntityManagerInterface $entityManager;
 
     /**
+     * @param EntityManagerInterface $entityManager
      * @param SessionInterface $session
      */
-    public function __construct(SessionInterface $session)
+    public function __construct(EntityManagerInterface $entityManager, SessionInterface $session)
     {
         $this->session = $session;
+        $this->entityManager = $entityManager;
+    }
+
+    public function cart(): array
+    {
+        $complete = [];
+
+        if ($this->get()) {
+            foreach ($this->get() as $id => $quantity) {
+                $product = $this->entityManager->getRepository(Product::class)->findOneById($id);
+
+                if (!$product) {
+                    $this->delete($id);
+                    continue;
+                }
+
+                $complete[] = [
+                    'product' => $product,
+                    'quantity' => $quantity
+                ];
+            }
+        }
+
+        return $complete;
     }
 
     public function add($id)
@@ -29,9 +57,31 @@ class Cart
         $this->session->set('cart', $cart);
     }
 
+    public function decrease($id)
+    {
+        $cart = $this->session->get('cart', []);
+
+        if ($cart[$id] > 1) {
+            $cart[$id]--;
+        } else {
+            unset($cart[$id]);
+        }
+
+        return $this->session->set('cart', $cart);
+    }
+
     public function get()
     {
         return $this->session->get('cart');
+    }
+
+    public function delete($id)
+    {
+        $cart = $this->session->get('cart', []);
+
+        unset($cart[$id]);
+
+        return $this->session->set('cart', $cart);
     }
 
     public function remove()
--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -3,40 +3,19 @@
 namespace App\Controller;
 
 use App\Classes\Cart;
-use App\Entity\Product;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CartController extends AbstractController
 {
-    private EntityManagerInterface $entityManager;
-
-    /**
-     * @param EntityManagerInterface $entityManager
-     */
-    public function __construct(EntityManagerInterface $entityManager)
-    {
-        $this->entityManager = $entityManager;
-    }
-
     /**
      * @Route("/panier", name="cart")
      */
     public function index(Cart $cart): Response
     {
-        $complete = [];
-
-        foreach ($cart->get() as $id => $quantity) {
-            $complete[] = [
-                'product' => $this->entityManager->getRepository(Product::class)->findOneById($id),
-                'quantity' => $quantity
-            ];
-        }
-
         return $this->render('cart/index.html.twig', [
-            'cart' => $complete
+            'cart' => $cart->cart()
         ]);
     }
 
@@ -46,6 +25,26 @@ class CartController extends AbstractController
     public function add($id, Cart $cart): Response
     {
         $cart->add($id);
+
+        return $this->redirectToRoute('cart');
+    }
+
+    /**
+     * @Route("/panier/supprimer-quantitee/{id}", name="cart_decrease")
+     */
+    public function decrease($id, Cart $cart): Response
+    {
+        $cart->decrease($id);
+
+        return $this->redirectToRoute('cart');
+    }
+
+    /**
+     * @Route("/panier/supprimer-produit/{id}", name="cart_delete")
+     */
+    public function delete($id, Cart $cart): Response
+    {
+        $cart->delete($id);
 
         return $this->redirectToRoute('cart');
     }
