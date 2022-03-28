06c79570504c539655d058e76be0dc8c749905a6
liohumb <helloliohumb@gmail.com>
2022-03-28 11:05:32 +0200

    mise en place de la gestion des transporteurs & corrextion template admin products

--- src/Controller/Admin/CarrierController.php
+++ src/Controller/Admin/CarrierController.php
@@ -2,19 +2,107 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Carrier;
+use App\Form\CarrierType;
+use App\Repository\CarrierRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CarrierController extends AbstractController
 {
+    private EntityManagerInterface $entityManager;
+
     /**
-     * @Route("/admin/carrier", name="app_admin_carrier")
+     * @param EntityManagerInterface $entityManager
      */
-    public function index(): Response
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * @Route("/administration/transporteurs", name="admin_carrier")
+     * @param CarrierRepository $carrierRepository
+     * @return Response
+     */
+    public function index(CarrierRepository $carrierRepository): Response
     {
         return $this->render('admin/carrier/index.html.twig', [
-            'controller_name' => 'CarrierController',
+            'carriers' => $carrierRepository->findAll()
         ]);
+    }
+
+    /**
+     * @Route("/administration/transporteurs/ajouter", name="admin_carrier_add")
+     * @param Request $request
+     * @return RedirectResponse|Response
+     */
+    public function add(Request $request): Response
+    {
+        $carrier = new Carrier();
+
+        $form = $this->createForm(CarrierType::class, $carrier);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $carrier = $form->getData();
+
+            $this->entityManager->persist($carrier);
+            $this->entityManager->flush();
+
+            return $this->redirectToRoute('admin_carrier');
+        }
+
+        return $this->render('admin/carrier/add.html.twig', [
+            'carrier' => $carrier,
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/administration/transporteurs/editer/{id}", name="admin_carrier_edit")
+     * @param Request $request
+     * @param Carrier $carrier
+     * @return RedirectResponse|Response
+     */
+    public function edit(Request $request, Carrier $carrier): Response
+    {
+        $form = $this->createForm(CarrierType::class, $carrier);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $carrier = $form->getData();
+
+            $this->entityManager->persist($carrier);
+            $this->entityManager->flush();
+
+            return $this->redirectToRoute('admin_carrier');
+        }
+
+        return $this->render('admin/carrier/edit.html.twig', [
+            'carrier' => $carrier,
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/administration/transporteurs/supprimer/{id}", name="admin_carrier_delete")
+     * @param $id
+     * @return RedirectResponse
+     */
+    public function delete($id): RedirectResponse
+    {
+        $carrier = $this->entityManager->getRepository(Carrier::class)->findOneById($id);
+
+        if ($carrier) {
+            $this->entityManager->remove($carrier);
+            $this->entityManager->flush();
+        }
+
+        return $this->redirectToRoute('admin_carrier');
     }
 }
--- src/Form/CarrierType.php
+++ src/Form/CarrierType.php
@@ -4,6 +4,9 @@ namespace App\Form;
 
 use App\Entity\Carrier;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,9 +15,36 @@ class CarrierType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('description')
-            ->add('price')
+            ->add('name', TextType::class, [
+                'label' => 'Le nom',
+                'attr' => [
+                    'class' => 'form__input',
+                    'placeholder' => ' '
+                ],
+                'label_attr' => [
+                    'class' => 'form__label'
+                ]
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'La description',
+                'attr' => [
+                    'class' => 'form__input',
+                    'placeholder' => ' '
+                ],
+                'label_attr' => [
+                    'class' => 'form__label'
+                ]
+            ])
+            ->add('price', NumberType::class, [
+                'label' => 'Le prix',
+                'attr' => [
+                    'class' => 'form__input',
+                    'placeholder' => ' '
+                ],
+                'label_attr' => [
+                    'class' => 'form__label'
+                ]
+            ])
         ;
     }
 
