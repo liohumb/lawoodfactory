acc08754b8961089bcadb48ad0b4d7ddc37a7f66
liohumb <helloliohumb@gmail.com>
2022-04-04 10:22:34 +0200

    mise en place du formulaire de contact

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Classes\Mail;
+use App\Form\ContactType;
 use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,12 +14,43 @@ class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
+     * @param ProductRepository $productRepository
+     * @param Request $request
+     * @return Response
      */
-    public function index(ProductRepository $productRepository): Response
+    public function index(ProductRepository $productRepository, Request $request): Response
     {
+        $notification = null;
+
+        $form = $this->createForm(ContactType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $notification = "Merci de nous avoir contacté. Nous vous répondrons dans les meilleurs délais.";
+
+            $mail = new Mail();
+            $mail->send(
+                'contact@lawoodfactory.fr',
+                'L&A WoodFactory',
+                'Vous avez un nouveau message • L&A WoodFactory',
+                "Bonjour Patron ! <br><br>
+                        J'ai l'immense honneur de vous annoncer que vous venez de recevoir un nouveau message par le biais du formulaire de contact. <br><br>
+                        C'est un message de <b>" . $form->getData()['name'] . "</b>. <br><br>
+                        Voici ce qu'il contient : <br>
+                        “<i>" . $form->getData()['content'] . "</i>” <br><br>
+                        Voici l'adresse email pour la réponse : <br>
+                        <b>" . $form->getData()['email'] ."</b><br><br><br>
+                        Je me permet de vous rappeler que si vous souhaitez répondre à ce message, un copier-coller du message de " . $form->getData()['name'] . " devra être fait par vous dans la réponse. <br><br>
+                        Il ne me reste plus qu'à vous souhaitez une bonne journée ! <br>
+                        BISOU BISOU !!"
+            );
+        }
+
         return $this->render('home/index.html.twig', [
             'productBest' => $productRepository->findByIsBest(1),
-            'productNew' =>$productRepository->findBy([], ['createdAt' => 'desc'], 4)
+            'productNew' =>$productRepository->findBy([], ['createdAt' => 'desc'], 4),
+            'form' => $form->createView(),
+            'notification' => $notification
         ]);
     }
 }
--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -3,6 +3,9 @@
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -11,7 +14,36 @@ class ContactType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('field_name')
+            ->add('name', TextType::class, [
+                'label' => 'Votre nom',
+                'attr' => [
+                    'class' => 'form__input',
+                    'placeholder' => ' '
+                ],
+                'label_attr' => [
+                    'class' => 'form__label'
+                ]
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'votre@email.com',
+                'attr' => [
+                    'class' => 'form__input',
+                    'placeholder' => ' '
+                ],
+                'label_attr' => [
+                    'class' => 'form__label'
+                ]
+            ])
+            ->add('content', TextareaType::class, [
+                'label' => 'Votre message',
+                'attr' => [
+                    'class' => 'form__input',
+                    'placeholder' => ' '
+                ],
+                'label_attr' => [
+                    'class' => 'form__label'
+                ]
+            ])
         ;
     }
 
