fabc1577b1f5b0a352d319c78238dda720754c73
liohumb <helloliohumb@gmail.com>
2022-04-05 07:10:23 +0200

    mise en place de la section service

--- src/Controller/ServiceController.php
+++ src/Controller/ServiceController.php
@@ -9,12 +9,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class ServiceController extends AbstractController
 {
     /**
-     * @Route("/service", name="app_service")
+     * @Route("/service", name="service")
      */
     public function index(): Response
     {
-        return $this->render('service/index.html.twig', [
-            'controller_name' => 'ServiceController',
-        ]);
+        return $this->render('service/index.html.twig');
     }
 }
