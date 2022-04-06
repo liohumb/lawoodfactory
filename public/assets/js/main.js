7a4f0dd6394c1c20ad02a36ff931d9d18ab83141
liohumb <helloliohumb@gmail.com>
2022-04-06 06:02:31 +0200

    correction js

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -49,32 +49,27 @@ let swiperPopular = new Swiper('.products__mobile-container', {
 /*===== SHADOW NAVBAR ON SCROLL =====*/
 window.addEventListener('scroll',(e)=>{
     const nav = document.getElementById('header');
-    if(window.pageYOffset>0){
+
+    if (window.pageYOffset>0) {
         nav.classList.add("header__shadow");
-    }else{
+    } else {
         nav.classList.remove("header__shadow");
     }
 });
 
 /*===== HIDE NAVBAR ON SCROLL =====*/
-var lastScrollTop; // This Varibale will store the top position
+var lastScrollTop;
 
-navbar = document.getElementById('header'); // Get The NavBar
+navbar = document.getElementById('header');
 
 window.addEventListener('scroll',function(){
-    //on every scroll this funtion will be called
-
     var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    //This line will get the location on scroll
 
-    if(scrollTop > lastScrollTop){ //if it will be greater than the previous
+    if (scrollTop > lastScrollTop) {
         navbar.style.top='-100px';
-        //set the value to the negetive of height of navbar
-    }
-
-    else{
+    } else {
         navbar.style.top='0';
     }
 
-    lastScrollTop = scrollTop; //New Position Stored
+    lastScrollTop = scrollTop;
 });
